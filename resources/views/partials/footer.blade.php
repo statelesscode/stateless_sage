4f2076b49840a3f088207a37274d47d735072bb2
Michael Duchemin <msducheminjr@users.noreply.github.com>
2021-10-30 10:48:52 -0400

    Visually identify links in the footer
    * Add styling classes to footer text links so that they are visually
    distinct from other text in the footer.
    * Make all links in the footer target="_blank" so they open in a new tab
    * Fix a couple of typos in the footer and modify language slightly
    
    Completes statelesscode.com backlog item:
    Visually identify hyperlinks in footer. Impossible to see.

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -9,27 +9,27 @@ $facebook_url = "https://www.facebook.com/statelesscode";
 $twitter_url = "https://twitter.com/statelesscode";
 $instagram_url = "https://www.instagram.com/statelesscode/";
 ?>
-<footer class="site-footer bg-gradient-to-br from-scseafoam-500 to-scseafoam-800 text-scbluegray-50 py-8 px-4 sm:px-12">
+<footer class="site-footer">
   <!-- Legal -->
   <div class="legal">
     <!-- ANTI IP ICON LINKS -->
     <div class="flex space-x-4 items-center justify-center">
       <div>
-        <a href="{{ $cc0_url }}">
+        <a href="{{ $cc0_url }}" target="_blank">
           <img alt="Creative Commons CC0 logo"
                src="@asset('images/cc-zero.svg')"
                class="h-6 flex-auto">
         </a>
       </div>
       <div>
-        <a href="{{ $unlicense_url }}">
+        <a href="{{ $unlicense_url }}" target="_blank">
           <img alt="UNLICENSE logo"
                src="@asset('images/unlicense-icon.svg')"
                class="h-6 flex-auto">
         </a>
       </div>
       <div>
-        <a href="{{ $mit_url }}">
+        <a href="{{ $mit_url }}" target="_blank">
           <img alt="MIT License logo"
                src="@asset('images/mit-license-icon.svg')"
                class="h-6 flex-auto">
@@ -38,48 +38,50 @@ $instagram_url = "https://www.instagram.com/statelesscode/";
     </div>
     <p class="text-xs text-gray-300 my-3 mx-4 sm:mx-12 md:mx-16 lg:mx-24 xl:mx-48 2xl:mx-64">
     Unless otherwise noted, all content on this site is
-    <a href="{{ $cc0_url }}">CC0 - No rights reserved</a>
-    and code is released under the <a href="{{ $unlicense_url }}">UNLICENSE</a> and/or the <a
-    href="{{ $unlicense_url }}">MIT Licence</a> (whichever you are more comfortable with).
-    Nearly all code is available on <a href="{{ $github_url }}">GitHub</a>. Stateless Code
-    is <a href="{{ $aip }}">against intellectual property</a> and considers it a racket.
-    Copying is not stealing. For logos to external sites, the copyright holders retain their
-    copyright. See their sites for more information.</p>
+    <a href="{{ $cc0_url }}" class="footer-text-link" target="_blank">CC0 - No rights reserved</a>
+    and code is released under the <a href="{{ $unlicense_url }}" target="_blank"
+    class="footer-text-link">UNLICENSE</a> and/or the <a href="{{ $mit_url }}" target="_blank"
+    class="footer-text-link">MIT License</a> (whichever you are more comfortable with). Nearly all
+    code is available on <a href="{{ $github_url }}" target="_blank"
+    class="footer-text-link">GitHub</a>. Stateless Code is <a href="{{ $aip }}" target="_blank"
+    class="footer-text-link">against intellectual property</a> and considers it a racket. Copying
+    is not stealing. For logos to external sites or labeled copyrighted material, the copyright
+    holders retain their copyright legal privileges. See their sites for more information.</p>
   </div>
   <!-- Social Media -->
   <div class="footer-social">
     <p class="text-center text-sm my-2">Follow us on social media.</p>
     <div class="flex space-x-4 items-center justify-center">
       <div>
-        <a href="{{ $github_url }}">
+        <a href="{{ $github_url }}" target="_blank">
           <img alt="GitHub logo"
                src="@asset('images/Octicons-mark-github.svg')"
                class="h-6 flex-auto">
         </a>
       </div>
       <div>
-        <a href="{{ $youtube_url }}">
+        <a href="{{ $youtube_url }}" target="_blank">
           <img alt="YouTube logo"
                src="@asset('images/youtube-logo.svg')"
                class="h-6 flex-auto">
         </a>
       </div>
       <div>
-        <a href="{{ $facebook_url }}">
+        <a href="{{ $facebook_url }}" target="_blank">
           <img alt="Facebook logo"
                src="@asset('images/facebook-logo.svg')"
                class="h-6 flex-auto">
         </a>
       </div>
       <div>
-        <a href="{{ $twitter_url }}">
+        <a href="{{ $twitter_url }}" target="_blank">
           <img alt="Twitter logo"
                src="@asset('images/twitter-logo.svg')"
                class="h-6 flex-auto">
         </a>
       </div>
       <div>
-        <a href="{{ $instagram_url }}">
+        <a href="{{ $instagram_url }}" target="_blank">
           <img alt="Instagram logo"
                src="@asset('images/instagram-logo.svg')"
                class="h-6 flex-auto">
